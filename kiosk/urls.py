a1a62fb2b742635a1cc98d926912df1babd6ce2b
InnovationPARK <innovation0220@gmail.com>
2024-04-08 16:52:30 +0900

    menu complete

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from kiosk_cafe import views
-
+from kiosk.views import HomeView
 
 
 urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
     path('admin/', admin.site.urls),
     path('kiosk_cafe/', include("kiosk_cafe.urls")), 
 ]
